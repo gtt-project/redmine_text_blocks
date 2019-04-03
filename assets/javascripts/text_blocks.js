e9330dfa626c0c80e336b3f637bbf49b634a7a68
Thibault <933kss@gmail.com>
2019-04-03 19:33:34 +0900

    fix null property error

--- assets/javascripts/text_blocks.js
+++ assets/javascripts/text_blocks.js
@@ -13,7 +13,7 @@ var TextBlocks = {
     if(value == '') return;
 
     console.log(value);
-    var fieldId = $('#textblock-select').parent().next('div.jstEditor').find('textarea').attr('id');
+    var fieldId = $('#textblock-select').parents().next('div.jstEditor').find('textarea').attr('id');
 
     var field = document.getElementById(fieldId);
     var field_ = $(field);
