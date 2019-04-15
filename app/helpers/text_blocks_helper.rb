530815787238a6a0ddc9f535eff6c97f49857d08
Thibault <933kss@gmail.com>
2019-04-15 17:07:34 +0900

    show text block options by issue status

--- app/helpers/text_blocks_helper.rb
+++ app/helpers/text_blocks_helper.rb
@@ -1,14 +1,26 @@
 module TextBlocksHelper
-  def text_block_options
+  def text_block_options(issue=nil)
     tags = []
     tags << content_tag(:option, value: '') do
       t('label_select_text_block')
     end
-    tags += TextBlock.where(project_id: [nil, @project.id]).to_a.map{|tb|
-      content_tag :option, value: tb.text do
-        tb.name
+    if issue
+      status_id = issue.status_id.to_s
+      txtblock_settings = Setting.plugin_redmine_text_blocks["textblock_config"][status_id]
+      if txtblock_settings
+        tags += TextBlock.where(project_id: [nil, @project.id]).to_a.map{|tb|
+          content_tag :option, value: tb.text do
+            tb.name
+          end if txtblock_settings.include?(tb.id.to_s)
+        }
       end
-    }
+    else
+      tags += TextBlock.where(project_id: [nil, @project.id]).to_a.map{|tb|
+        content_tag :option, value: tb.text do
+          tb.name
+        end
+      }
+    end
     safe_join tags
   end
 end
