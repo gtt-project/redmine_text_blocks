64a93146c1b48208072dfbb2c6dca834cc4555f4
Thibault <933kss@gmail.com>
2019-04-16 19:33:57 +0900

    add IssueStatus model override

--- lib/redmine_text_blocks.rb
+++ lib/redmine_text_blocks.rb
@@ -3,6 +3,7 @@ require 'redmine_text_blocks/view_hooks'
 module RedmineTextBlocks
   def self.setup
     RedmineTextBlocks::ProjectPatch.apply
+    RedmineTextBlocks::IssueStatusPatch.apply
     ProjectsController.send :helper, RedmineTextBlocks::ProjectSettingsTabs
     IssuesController.send :helper, TextBlocksHelper
   end
