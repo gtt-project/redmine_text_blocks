f4bf2ea5e11b15064bf0b7309866aa072340f482
Ko Nagase <nagase@georepublic.co.jp>
2022-04-11 14:01:10 +0900

    Supported Redmine 4.2

--- init.rb
+++ init.rb
@@ -5,6 +5,7 @@ if Rails.version > '6.0' && Rails.autoloaders.zeitwerk_enabled?
     RedmineTextBlocks.setup
   end
 else
+  require 'redmine_text_blocks'
   Rails.configuration.to_prepare do
     RedmineTextBlocks.setup
   end
